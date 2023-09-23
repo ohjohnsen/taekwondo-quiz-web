296cb68fb15a802462a4278a2eca2c1e7c969397
Øystein Holvik Johnsen <ohj@scanreach.com>
2023-09-23 07:58:26 +0200

    Add color + icon to answer buttons

--- src/pages/GuessTerminology.jsx
+++ src/pages/GuessTerminology.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { terminologies } from '../assets/terminologies';
-import { Box, Stack, Button, IconButton } from "@chakra-ui/react";
-import { MdRefresh } from 'react-icons/md';
+import { Flex, Box, Stack, Button, Icon, IconButton, Grid } from "@chakra-ui/react";
+import { MdRefresh, MdCheckCircle } from 'react-icons/md';
 
 const getKoreanQuestionAndNorwegianChoices = () => {
   console.log("Generating guesses");
@@ -57,52 +57,72 @@ const GuessTerminology = () => {
   const [selectedAnswer, setSelectedAnswer] = useState(undefined);
 
   return (
-    <Box>
-      Guess terminology
-      <Box>
-        Question { currentQuestion }:
-        { guessingData.question.terminology }
-      </Box>
-      <Stack>
-        { guessingData.choices.map(choice =>
-          <Button
-            key={choice.index}
-            value={choice.index}
-            colorScheme={ selectedAnswer !== choice.index ? 'gray' :
-              selectedAnswer === guessingData.question.index ? 'green' : 'red' }
-            onClick={() => {
-              if (selectedAnswer === undefined) {
-                console.log(choice.index);
-                setSelectedAnswer(choice.index);
-              }
-              setAnswers([
-                ...answers,
-                choice.index === guessingData.question.index
-              ])
-            }}
-          >
-            {choice.terminology}
-          </Button>) }
-      </Stack>
-      { (selectedAnswer !== undefined || currentQuestion > 1) &&
+    <Flex
+      background='cyan.900'
+      height='100vh'
+      width='100vw'
+      padding='1rem'
+      flexDirection='column'
+      alignItems='center'
+    >
+      <Box background='cyan.50' padding='1rem' width='50rem' borderRadius='0.5rem' dropShadow=''>
+        Guess terminology
         <Box>
-          { calculateCorrectAnswerPercentage(answers) } %
+          Question { currentQuestion }:
+          { guessingData.question.terminology }
         </Box>
-      }
-      { selectedAnswer !== undefined &&
-        <IconButton
-          visibility={selectedAnswer === undefined}
-          icon={<MdRefresh />}
-          isRound={true}
-          onClick={() => {
-            guessingData = getKoreanQuestionAndNorwegianChoices();
-            const nextQuestion = currentQuestion + 1;
-            setCurrentQuestion(nextQuestion);
-            setSelectedAnswer(undefined);
-          }}
-        />
-      }
-    </Box>
+        <Stack>
+          { guessingData.choices.map(choice =>
+            <Button
+              key={choice.index}
+              value={choice.index}
+              colorScheme={ selectedAnswer !== choice.index ? 'gray' :
+                selectedAnswer === guessingData.question.index ? 'green' : 'red' }
+              onClick={() => {
+                if (selectedAnswer === undefined) {
+                  console.log(choice.index);
+                  setSelectedAnswer(choice.index);
+                  setAnswers([
+                    ...answers,
+                    choice.index === guessingData.question.index
+                  ]);
+                }
+              }}
+            >
+              <Grid height='100%' width='100%' alignItems='center'>
+                {choice.terminology}
+                { selectedAnswer !== undefined &&
+                  selectedAnswer !== choice.index &&
+                  choice.index === guessingData.question.index &&
+                  <Icon color='green' as={MdCheckCircle} boxSize='1.8rem' style={{
+                    position: 'absolute',
+                    right: '0',
+                    marginRight: '1rem'
+                  }} />
+                }
+              </Grid>
+            </Button>) }
+        </Stack>
+        { (selectedAnswer !== undefined || currentQuestion > 1) &&
+          <Box>
+            { calculateCorrectAnswerPercentage(answers) } %
+          </Box>
+        }
+        { selectedAnswer !== undefined &&
+          <IconButton
+            visibility={selectedAnswer === undefined}
+            icon={<MdRefresh />}
+            isRound={true}
+            onClick={() => {
+              guessingData = getKoreanQuestionAndNorwegianChoices();
+              const nextQuestion = currentQuestion + 1;
+              setCurrentQuestion(nextQuestion);
+              setSelectedAnswer(undefined);
+            }}
+          />
+        }
+      </Box>
+    </Flex>
   );
 };
 
