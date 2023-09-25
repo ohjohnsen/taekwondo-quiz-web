d5a18936828c5d19cce7b048ec24bf15c5381dbb
Øystein Holvik Johnsen <ohj@scanreach.com>
2023-09-25 09:32:01 +0200

    Adding 10. Gup terminology list

--- src/assets/terminologies.js
+++ src/assets/terminologies.js
@@ -31,4 +31,164 @@ export const terminologies = [
     norwegian: "Skrik",
     korean: "Kihap"
   },
+  {
+    norwegian: "Tilbake til utgangspunkt",
+    korean: "Parou"
+  },
+  {
+    norwegian: "Slapp av",
+    korean: "Swiyo"
+  },
+  {
+    norwegian: "Tre av",
+    korean: "Hae san"
+  },
+  {
+    norwegian: "Stilling",
+    korean: "Sogi"
+  },
+  {
+    norwegian: "Sittestilling",
+    korean: "Annun sogi"
+  },
+  {
+    norwegian: "Gå-stilling",
+    korean: "Gunnun sogi"
+  },
+  {
+    norwegian: "Punch",
+    korean: "Jirugi"
+  },
+  {
+    norwegian: "Blokk",
+    korean: "Makgi"
+  },
+  {
+    norwegian: "Spark",
+    korean: "Chagi"
+  },
+  {
+    norwegian: "Lav",
+    korean: "Najunde"
+  },
+  {
+    norwegian: "Middels",
+    korean: "Kaunde"
+  },
+  {
+    norwegian: "Høy",
+    korean: "Nopunde"
+  },
+  {
+    norwegian: "Rett",
+    korean: "Baro"
+  },
+  {
+    norwegian: "Front",
+    korean: "Ap"
+  },
+  {
+    norwegian: "Side",
+    korean: "Yop"
+  },
+  {
+    norwegian: "Innsiden",
+    korean: "An"
+  },
+  {
+    norwegian: "Underarm",
+    korean: "Palmok"
+  },
+  {
+    norwegian: "Knivhånd",
+    korean: "Sonkal"
+  },
+  {
+    norwegian: "Knivfot",
+    korean: "Balkal"
+  },
+  {
+    norwegian: "Knyttneve",
+    korean: "Joomuk"
+  },
+  {
+    norwegian: "Hånd",
+    korean: "Son"
+  },
+  {
+    norwegian: "Fot",
+    korean: "Bal"
+  },
+  {
+    norwegian: "Oppadstigende",
+    korean: "Olligi"
+  },
+  {
+    norwegian: "Firevending",
+    korean: "Saju"
+  },
+  {
+    norwegian: "Selvforsvar",
+    korean: "Ho sin sul"
+  },
+  {
+    norwegian: "Sparring",
+    korean: "Matsogi"
+  },
+  {
+    norwegian: "Ass. instr. (1. - 3. Dan)",
+    korean: "Boo sabumnim"
+  },
+  {
+    norwegian: "Instruktør (4. - 6. Dan)",
+    korean: "Sabumnim"
+  },
+  {
+    norwegian: "Master (7. - 8. Dan)",
+    korean: "Sahyunnim"
+  },
+  {
+    norwegian: "Grand master (9. Dan)",
+    korean: "Sasungnim"
+  },
+  {
+    norwegian: "En",
+    korean: "Hana"
+  },
+  {
+    norwegian: "To",
+    korean: "Dul"
+  },
+  {
+    norwegian: "Tre",
+    korean: "Set"
+  },
+  {
+    norwegian: "Fire",
+    korean: "Net"
+  },
+  {
+    norwegian: "Fem",
+    korean: "Dasot"
+  },
+  {
+    norwegian: "Seks",
+    korean: "Yosot"
+  },
+  {
+    norwegian: "Sju",
+    korean: "Ilgop"
+  },
+  {
+    norwegian: "Åtte",
+    korean: "Yodul"
+  },
+  {
+    norwegian: "Ni",
+    korean: "Ahop"
+  },
+  {
+    norwegian: "Ti",
+    korean: "Yol"
+  },
 ];
